305dbd5a96ed18adaca393bf6a61dcf08974aefc
VincentXis <rickard.bernal@gmail.com>
2016-11-28 02:00:39 +0100

    removed while-loop and moved the message handling to the switch-tree. looks better now.

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -14,7 +14,7 @@ public class AddressBookMain {
      * I decided to keep the logging.properties file in src, since im only sending the src folder.
      * And my file is slightly different from the one provided initially.
      * the log file does not write anything to the console, and does not overwrite the file each time its run.
-     *
+     * however like your file, it does log to a folder called "logs" in the project folder.
      */
     private static void loggerSetup() {
         String loggingFilePath = "src/logging.properties";
